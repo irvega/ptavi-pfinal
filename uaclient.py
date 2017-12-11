ec00478552e7ed0b8db1547478d82066b53dd61e
Irene Vega Lago <ivega@gamma18.aulas.gsyc.urjc.es>
2017-12-11 09:42:59 +0100

    Añadido

---
+++ uaclient.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Programa cliente que abre un socket a un servidor
+"""
+
+import socket
+import sys
+
+METODO = sys.argv[1]
+
+class XML():
+    def fichero():
+
+
