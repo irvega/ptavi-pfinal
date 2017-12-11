00a13eb088719d027e4be6eccdd2d11586cf89aa
Irene Vega Lago <ivega@beta06.aulas.gsyc.urjc.es>
2017-12-11 15:33:05 +0100

    XML

--- uaclient.py
+++ uaclient.py
@@ -4,12 +4,36 @@
 Programa cliente que abre un socket a un servidor
 """
 
+from xml.sax import make_parser
+from xml.sax.handler import ContentHandler
 import socket
 import sys
 
-METODO = sys.argv[1]
+#METODO = sys.argv[1]
 
-class XML():
-    def fichero():
+class XML(ContentHandler):
+    def __init__(self):
+        self.dic = {}
+        self.label = {'account': ['username', 'password'],
+                      'uaserver':  ['ip', 'port'],
+                      'rtpaudio': ['port'],
+                      'regproxy': ['ip', 'port'],
+                      'log': ['path'],
+                      'audio': ['path']
+                      }
+    def startElement(self, name, attrs):
+        if name in self.label:
+            for atrib in self.label[name]:
+                self.dic[name + "_" + atrib] = attrs.get(atrib, "")
+            IP = self.dic['uaserver_ip']
+            PORT = self.dic['uaserver_port']
+    def dictio(self):
+        return(self.dic)
 
-
+    #def log():
+if __name__ == "__main__":
+    parser = make_parser()
+    archivo=XML()
+    parser.setContentHandler(archivo)
+    parser.parse(open('ua2.xml'))
+    print(archivo.dictio())
