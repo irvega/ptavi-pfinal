1ee218b6f11e9efeaa8977cf13ea056ea7487659
irvega <irene@machine.com>
2018-01-04 15:37:11 +0100

    log

--- uaclient.py
+++ uaclient.py
@@ -2,13 +2,17 @@
 # -*- coding: utf-8 -*-
 
 from random import randint
+from time import gmtime, strftime, time
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 import socket
 import sys
 
+
 class XML(ContentHandler):
+
     dic = {}
+
     def __init__(self):
         """
         Crea un diccionario
@@ -18,8 +22,10 @@ class XML(ContentHandler):
                       'rtpaudio': ['port'],
                       'regproxy': ['ip', 'port'],
                       'log': ['path'],
-                      'audio': ['path']
+                      'audio': ['path'],
+                      'server':['name', 'ip', 'port']
                       }
+
     def startElement(self, name, attrs):
         """
         Guarda de name_atri
@@ -27,39 +33,67 @@ class XML(ContentHandler):
         if name in self.label:
             for atrib in self.label[name]:
                 self.dic[name + "_" + atrib] = attrs.get(atrib, "")
+
     def dictio(self):
         """
         Devuelve el diccionario
         """
         return(self.dic)
+
     def parse():
         """
         Lee el fichero
         """
         CONFIG = sys.argv[1] #ua1.xml
         parser = make_parser()
-        archivo=XML()
+        archivo = XML()
         parser.setContentHandler(archivo)
         parser.parse(open(CONFIG))
         confdict = archivo.dictio()
-        
-if __name__ == "__main__":
-    if len(sys.argv)!=4:
-        sys.exit('Usage: python uaclient.py config method option')
-    METODO = sys.argv[2]
-    LINE = sys.argv[3]
 
-    def log(logfile, tipo, ip, message):
-        """
-        Escribe en un fichero
-        """
-        df = open('logfile', 'a')
+if __name__ == "__main__":
+
+    if len(sys.argv) != 4:
+        sys.exit('Usage: python uaclient.py config method option')
 
     XML.parse()
     IP = XML.dic['uaserver_ip']
-    PORT = int(XML.dic['uaserver_port'])   
+    PORT = int(XML.dic['uaserver_port'])
     USER = XML.dic['account_username']
     PORT_AUDIO = XML.dic['rtpaudio_port']
+    archivo = XML()
+    confdict = archivo.dictio()
+
+    METODO = sys.argv[2]
+    LINE = sys.argv[3]
+    #SENT = IP + ':' + PORT + ': '
+    def timenow():
+        """
+        Tiempo actual
+        """
+        timereal = strftime("%Y%m%d%H%M%S", gmtime(time()))
+        return timereal
+    #def log(logfile, tipo, ip, message):
+
+        #try:
+    logfile = open('logfile.txt', "a")
+    logfile.write(str(timenow()) + " Starting...\n")
+        #except FileNotFoundError:
+        #    logfile = open('logfile', "w")
+
+    def logsent(logfile):
+        """
+        Escribe en el log lo que envio
+        """
+        logfile.write(str(timenow()) + " Sent to " + IP + ':' + str(PORT) + ': ' +
+                      message + "\n")
+
+    def logrecive(logfile):
+        """
+        Escribe en el log lo que recivo
+        """
+        logfile.write(str(timenow()) + " Recived from " + IP + ':' + str(PORT) +
+                      ': ' + str(''.join(RECIVE[1:-2]) + "\n"))
 
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
         my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -68,30 +102,38 @@ if __name__ == "__main__":
         if METODO == 'REGISTER':
             message = 'REGISTER sip:'+USER+' SIP/2.0\r\nExpires: '+LINE+'\r\n'
             my_socket.send(bytes(message + '\r\n', 'utf-8') + b'\r\n')
-            #log(confdict['logfile'], 'sent', IP, message,'\r\n')
         elif METODO == 'INVITE':
-            message = ('INVITE sip:'+USER+' SIP/2.0\r\n' + 'Content-Type: ' +
-                       'application/sdp\r\n\r\n' + 'v=0\r\no=' + USER + ' ' + 
+            message = ('INVITE sip:'+USER+ ' SIP/2.0\r\n' + 'Content-Type: ' +
+                       'application/sdp\r\n\r\n' + 'v=0\r\no=' + USER + ' ' +
                        str(PORT) + '\r\ns=misesion\r\nt=0\r\nm=audio ' +
                        PORT_AUDIO + ' RTP\r\n\r\n')
             my_socket.send(bytes(message, 'utf-8')+b'\r\n\r\n')
         elif METODO == 'BYE':
-            my_socket.send(bytes('BYE sip:' + LINE + ' SIP/2.0\r\n', 'utf-8') +
-                           b'\r\n')
+            message =  ('BYE sip:' + LINE + ' SIP/2.0\r\n')
+            my_socket.send(bytes(message, 'utf-8') + b'\r\n')
         else:
             my_socket.send(bytes(METODO + ' sip: ' + LINE +
                                  ' SIP/2.0\r\n', 'utf-8') + b'\r\n\r\n')
+        try:
+            DATA = my_socket.recv(1024)
+        except ConnectionRefusedError:
+            sys.exit('20101018160243 Error: No server listening at '+ IP +
+                     ' port ' + str(PORT))
+        logsent(logfile)
 
-        DATA = my_socket.recv(1024)
         print('Recibido:', DATA.decode('utf-8'))
         RECIVE = DATA.decode('utf-8').split(' ')
+        logrecive(logfile)
+
         for element in RECIVE:
             if element == '401':
-                my_socket.send(bytes(message + 
-                               'Authorization: Digest response="' + 
-                               str(randint(0,99999999999999999)) +
+                my_socket.send(bytes(message +
+                               'Authorization: Digest response="' +
+                               str(randint(0, 99999999999999999)) +
                                '"\r\n', 'utf-8') + b'\r\n')
             if element == '200' and METODO == 'INVITE':
                 my_socket.send(bytes('ACK sip:' + USER +
                                      ' SIP/2.0\r\n', 'utf-8') + b'\r\n')
-
+            if element == '200' and METODO == 'BYE':
+                logfile.write(str(timenow()) + " Finishing.\n")
+                logfile.close()
