6a4514913f886b2ecc4a3a2636da7dc3d02cc806
irvega <irene@machine.com>
2018-01-10 22:18:18 +0100

    Con log

--- uaserver.py
+++ uaserver.py
@@ -4,11 +4,15 @@ from random import randint
 from uaclient import XML, log
 from xml.sax import make_parser
 import os
+import socket
 import socketserver
 import sys
 
 
 class EchoHandler(socketserver.DatagramRequestHandler):
+
+    dic = {}
+
     def error(self, line):
         """
         Busca errores en la petición
@@ -38,35 +42,49 @@ class EchoHandler(socketserver.DatagramRequestHandler):
             if method not in lista:
                 message = 'SIP/2.0 405 Method Not Allowed\r\n\r\n'
                 self.wfile.write(bytes(message, 'utf-8'))
-                log.logsent(IP, PORT, message)
+                #log.logsent(IP, PORT, message)
             elif self.error(line.decode('utf-8')):
                 message = 'SIP/2.0 400 Bad Request\r\n\r\n'
                 self.wfile.write(bytes(message, 'utf-8'))
-                log.logsent(IP, PORT, message)
+                #log.logsent(IP, PORT, message)
             elif method == lista[0]:
-                message = ('SIP/2.0 100 Trying \r\n\r\n' + 
+                message = ('SIP/2.0 100 Trying \r\n\r\n' +
                            'SIP/2.0 180 Ringing \r\n\r\n' +
-                           'SIP/2.0 200 OK  \r\n\r\n' + 
+                           'SIP/2.0 200 OK  \r\n\r\n' +
                            'Content-Type: application/sdp\r\n\r\n' +
                            'v=0\r\no=' + USER + ' ' + IP + '\r\ns=ven' +
-                           'gadores\r\nt=0\r\nm=audio '+
-                            str(PORT_RTP) + ' RTP\r\n\r\n')
+                           'gadores\r\nt=0\r\nm=audio ' + str(PORT_RTP) +
+                           ' RTP\r\n\r\n')
                 self.wfile.write(bytes(message, 'utf-8'))
-                log.logsent(IP, PORT, message) 
-                #FALTA SDP 
+                #log.logsent(IP, PORT, message)
+                USER_CL = (line.decode('utf-8').split(' ')[3].split('=')[2])
+                print(USER_CL)
                 PORT_CL = (line.decode('utf-8').split(' ')[5])
                 IP_CL = (line.decode('utf-8').split(' ')[4].split('\r\n')[0])
+                self.dic[USER_CL] = [IP_CL, PORT_CL]
             elif method == lista[1]:
                 message = 'SIP/2.0 200 OK  \r\n\r\n'
                 self.wfile.write(bytes(message, 'utf-8'))
-                log.logsent(IP, PORT, message) 
+                #log.logsent(IP, PORT, message)
                 log.cierre()
             elif method == lista[2]:
-                aEjecutar = "./mp32rtp -i " + IP_CL + " -p " + str(PORT_CL)
-                aEjecutar += " < " + CANCION
-                print("Enviamos RTP: ", aEjecutar)
-                os.system(aEjecutar)
+                print('AAAACKKKK_serv')
+                print(self.dic[USER_CL][0])
+                print(self.dic[USER_CL][1])
+                if USER in self.dic: #COMPROBAAR
+                    print(self.dic[USER_CL][1])
+                    aEjecutar = ("./mp32rtp -i " + self.dic[USER_CL][0] + " -p " +
+                                 str(self.dic[USER_CL][1]))
+                    aEjecutar += " < " + CANCION
+                    print("Enviamos RTP: ", aEjecutar)
+                    os.system(aEjecutar)
+            print(message)
+            log.logsent(IP, PORT, message)
             print(' The client send:\r\n' + line.decode('utf-8'))
+            RECIVE = line.decode('utf-8').split(' ')
+            #print(RECIVE)
+            log.logrecive(IP, PORT, RECIVE)
+            
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -79,13 +97,6 @@ if __name__ == "__main__":
     USER = XML.dic['account_username']
     PORT_RTP = int(XML.dic['rtpaudio_port'])
     fichero = XML.dic['log_path']
-
-    def timenow():
-        """
-        Tiempo actual
-        """
-        timereal = strftime("%Y%m%d%H%M%S", gmtime(time()))
-        return timereal
 
     log = log(fichero)
     SERV = socketserver.UDPServer((IP, PORT), EchoHandler)
