f5618cac37f5ca8063d7875dc5cdc65594305321
Irene Vega Lago <ivega@alpha23.aulas.gsyc.urjc.es>
2018-01-11 17:09:27 +0100

    Correcion

--- proxy_registrar.py
+++ proxy_registrar.py
@@ -31,7 +31,6 @@ class USERS(socketserver.DatagramRequestHandler):
             file.write(line)
             """
             file.write(str(self.dic).replace('], ', '\r\n'))
-            #file.write(' '.join(self.dic))
 
     def register(self):
         """
@@ -51,12 +50,12 @@ class USERS(socketserver.DatagramRequestHandler):
         expired = []
         time_exp = strftime('%Y-%m-%d %H:%M:%S', gmtime(time()))
         for USER in self.dic:
-            if self.dic[USER][3] <= time_exp:
+            if str(self.dic[USER][2]) <= time_exp:
                 expired.append(USER)
         for USER in expired:
             del self.dic[USER]
 
-    def keyfile(self, USER, psw_file):  #METEER path
+    def keyfile(self, USER, psw_file):
         """
         Escribe en un fichero usuario y contraseña
         """
@@ -109,6 +108,9 @@ class USERS(socketserver.DatagramRequestHandler):
         line = self.rfile.read()
         WORD = line.decode('utf-8').split()
         METHOD = WORD[0]
+
+        RECIVE = line.decode('utf-8').split(' ')
+
         if line and line.decode('utf-8')[:8] == 'REGISTER':
             USER = WORD[1][4:-5]
             IP = self.client_address[0]
@@ -116,71 +118,73 @@ class USERS(socketserver.DatagramRequestHandler):
             EXPIRE_NUM = WORD[4]
             EXPIRES = int(EXPIRE_NUM)+time()
             EXPIRE = strftime('%Y-%m-%d %H:%M:%S', gmtime(EXPIRES))
+            log.logrecive(IP, PORT,  RECIVE, fichero)
             if WORD[4] != '0':
                 if USER in self.dic:
                     print('Ya registrado: ', EXPIRE_NUM)
-                    self.dic[USER] = [IP, PORT, EXPIRES, EXPIRE, EXPIRE_NUM]
+                    self.dic[USER] = [IP, PORT, EXPIRES, EXPIRE_NUM]
                     message = 'SIP/2.0 200 OK  \r\n\r\n'
                     self.wfile.write(bytes(message, 'utf-8'))
-                    #log.logsent(IP, PORT, message)
                 else:
                     if line.decode('utf-8').split('\r\n')[2]:
                         if WORD[5][0:-1] == 'Authorization':
                             NUM_CLIENT = WORD[7][10:-1]
                             NUM_PROXY = self.check(self.dic_nonc[USER], USER)
                             if NUM_CLIENT == NUM_PROXY:
-                                self.dic[USER] = [IP, PORT, EXPIRES, EXPIRE, 
-                                                    EXPIRE_NUM]
+                                self.dic[USER] = [IP, PORT, EXPIRES, EXPIRE_NUM]
                                 message = 'SIP/2.0 200 OK  \r\n\r\n'
                                 self.wfile.write(bytes(message, 'utf-8'))
-                                #log.logsent(IP, PORT, message)
                                 self.user_create(base)
                             else:
                                 message = 'SIP/2.0 400 Bad Request\r\n\r\n'
                                 self.wfile.write(bytes(message, 'utf-8'))
-                                #log.logsent(IP, PORT, message)
                     else:
                         nonce = randint(0, 99999999999999999)
                         self.dic_nonc[USER] = str(nonce)
                         message = ('SIP/2.0 401 Unauthorized\r\n' +
-                                  'WWW Authenticate: Digest nonce="' +
+                                  'WWW-Authenticate: Digest nonce="' +
                                    str(nonce))
                         self.wfile.write(bytes(message, 'utf-8') + b'" \r\n')
-                        #log.logsent(IP, PORT, message)
             else:
-                self.dic[USER] = [IP, PORT, EXPIRES, EXPIRE, EXPIRE_NUM]
+                self.dic[USER] = [IP, PORT, EXPIRES, EXPIRE_NUM]
                 try:
                     del self.dic[USER]
                     self.user_create(base)
-                    print(' USER DELETE')
+                    message = (' USER DELETE')
                 except(KeyError):
-                    self.wfile.write(b' NOTICE: This user dont exist!')
+                    message = (' NOTICE: This user dont exist!')
+            log.logsent(IP, PORT, message, fichero)
         elif line and METHOD == 'INVITE' or METHOD == 'BYE' or METHOD == 'ACK':
             USER = WORD[1][4:]
             if USER in self.dic:
                 #PORT_SERV = int('7002')
-                IP_SERV = self.dic[USER][0]
-                PORT_SERV = int(self.dic[USER][1])
-                message = self.sent_uaserver(IP_SERV, PORT_SERV, METHOD, line)
+                IP = self.dic[USER][0] #SERV
+                PORT = int(self.dic[USER][1]) #SERV
+                message = self.sent_uaserver(IP, PORT, METHOD, line)
                 self.wfile.write(bytes(message, 'utf-8') + b'\r\n\r\n')
-                #log.logrecive(IP, PORT, message)
             else:
                 message = 'SIP/2.0 404 User Not Found\r\n\r\n'
                 self.wfile.write(bytes(message, 'utf-8'))
-                #log.logsent(IP, PORT, message)
             if METHOD == 'ACK':
                 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
                     print('AAACKKKK')
-                    print(IP_SERV)
-                    print('puerto: ', PORT_SERV)
+                    print(IP)
+                    print('puerto: ', PORT)
+                    PORT1 = 7001
                     my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-                    my_socket.connect((IP_SERV, PORT_SERV))
+                    my_socket.connect((IP, PORT1))
                     my_socket.send(bytes(line.decode('utf-8'), 'utf-8') + b'\r\n')
+            if RECIVE != None:
+                IP = self.client_address[0] #PORT COREEGIIIIR
+                log.logrecive(IP, PORT,  RECIVE, fichero)
+            log.logsent(IP, PORT, message, fichero)
         else:
+            IP = self.client_address[0] #PONER
+            PORT = self.client_address[1]
             message = 'SIP/2.0 405 Method Not Allowed \r\n\r\n'
             self.wfile.write(bytes(message, 'utf-8'))
-            #log.logsent(IP, PORT, message)
-        log.logsent(IP, PORT, message)
+            log.logrecive(IP, PORT,  RECIVE, fichero)
+            log.logsent(IP, PORT, message, fichero)
         print(line.decode('utf-8'), end='')
         print(self.dic)
 
